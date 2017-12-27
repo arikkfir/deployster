b9d6d7d2d40b99ef895526754d37839d1ade44f4
Arik Kfir <arik@infolinks.com>
2017-12-28 00:30:29 +0200

    Run tests in build

---
+++ run-tests.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [[ "THIS_WILL" == "NEVER_BE_TRUE" ]]; then
+    CIRCLECI="${CIRCLECI}"
+fi
+
+PYTHONPATH="$(pwd)/src:$(pwd)/resources/src" \
+    py.test --cov-config=./.coveragerc \
+            --cov-report=term-missing \
+            --cov-branch \
+            --cov=./src/ \
+            --cov=./resources/src/ \
+            --showlocals \
+            --capture=fd \
+            -vv \
+            tests/ \
+            $@
+[[ $? != 0 ]] && echo "Tests failed!" >&2 && exit 1
+
+if [[ -n "${CIRCLECI}" ]]; then
+    coveralls
+    [[ $? != 0 ]] && echo "Coverage reporting failed!" >&2 && exit 1
+fi
+
+exit 0
