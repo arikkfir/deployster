55ad7855efffd6d317f2c05a6cdfb91797f21cda
Arik Kfir <arik@infolinks.com>
2017-12-29 01:26:37 +0200

    Auto-expand build sections, and make Docker build more verbose

--- .buildkite/build-branch.sh
+++ .buildkite/build-branch.sh
@@ -4,23 +4,23 @@
 set -e
 
 # process Dockerfile files and replace ":local" in "FROM" clauses with actual tag
-echo "--- Updating FROM directives in Dockerfile(s)"
+echo "+++ Updating FROM directives in Dockerfile(s)"
 for dockerfile in $(ls -X ./resources/Dockerfile.*); do
     sed -i "s/^FROM \(infolinks\/deployster-[^:]\+\):.\+$/FROM \1:${BUILDKITE_COMMIT}/g" ${dockerfile}
 done
 
 # build resource images
-echo "--- Building resource images"
-docker build -q --tag quay.io/kfirs/deployster-dresource:${BUILDKITE_COMMIT} -f ./resources/Dockerfile.dresource ./resources
-docker build -q --tag quay.io/kfirs/deployster-gcp:${BUILDKITE_COMMIT} -f ./resources/Dockerfile.gcp ./resources
-docker build -q --tag quay.io/kfirs/deployster-gcp-cloud-sql:${BUILDKITE_COMMIT} -f ./resources/Dockerfile.gcp_cloud_sql ./resources
-docker build -q --tag quay.io/kfirs/deployster-gcp-compute-ip-address:${BUILDKITE_COMMIT} -f ./resources/Dockerfile.gcp_compute_ip_address ./resources
-docker build -q --tag quay.io/kfirs/deployster-gcp-gke-cluster:${BUILDKITE_COMMIT} -f ./resources/Dockerfile.gcp_gke_cluster ./resources
-docker build -q --tag quay.io/kfirs/deployster-gcp-iam-policy:${BUILDKITE_COMMIT} -f ./resources/Dockerfile.gcp_iam_service_account ./resources
-docker build -q --tag quay.io/kfirs/deployster-gcp-iam-service-account:${BUILDKITE_COMMIT} -f ./resources/Dockerfile.gcp_iam_policy ./resources
-docker build -q --tag quay.io/kfirs/deployster-gcp-project:${BUILDKITE_COMMIT} -f ./resources/Dockerfile.gcp_project ./resources
-docker build -q --tag quay.io/kfirs/deployster-k8s:${BUILDKITE_COMMIT} -f ./resources/Dockerfile.k8s ./resources
-docker build -q --build-arg "VERSION=${BUILDKITE_COMMIT}" --tag quay.io/kfirs/deployster:${BUILDKITE_COMMIT} -f ./Dockerfile .
+echo "+++ Building resource images"
+docker build --tag quay.io/kfirs/deployster-dresource:${BUILDKITE_COMMIT} -f ./resources/Dockerfile.dresource ./resources
+docker build --tag quay.io/kfirs/deployster-gcp:${BUILDKITE_COMMIT} -f ./resources/Dockerfile.gcp ./resources
+docker build --tag quay.io/kfirs/deployster-gcp-cloud-sql:${BUILDKITE_COMMIT} -f ./resources/Dockerfile.gcp_cloud_sql ./resources
+docker build --tag quay.io/kfirs/deployster-gcp-compute-ip-address:${BUILDKITE_COMMIT} -f ./resources/Dockerfile.gcp_compute_ip_address ./resources
+docker build --tag quay.io/kfirs/deployster-gcp-gke-cluster:${BUILDKITE_COMMIT} -f ./resources/Dockerfile.gcp_gke_cluster ./resources
+docker build --tag quay.io/kfirs/deployster-gcp-iam-policy:${BUILDKITE_COMMIT} -f ./resources/Dockerfile.gcp_iam_service_account ./resources
+docker build --tag quay.io/kfirs/deployster-gcp-iam-service-account:${BUILDKITE_COMMIT} -f ./resources/Dockerfile.gcp_iam_policy ./resources
+docker build --tag quay.io/kfirs/deployster-gcp-project:${BUILDKITE_COMMIT} -f ./resources/Dockerfile.gcp_project ./resources
+docker build --tag quay.io/kfirs/deployster-k8s:${BUILDKITE_COMMIT} -f ./resources/Dockerfile.k8s ./resources
+docker build --build-arg "VERSION=${BUILDKITE_COMMIT}" --tag quay.io/kfirs/deployster:${BUILDKITE_COMMIT} -f ./Dockerfile .
 docker tag quay.io/kfirs/deployster-k8s:${BUILDKITE_COMMIT} quay.io/kfirs/deployster-k8s-clusterrole:${BUILDKITE_COMMIT}
 docker tag quay.io/kfirs/deployster-k8s:${BUILDKITE_COMMIT} quay.io/kfirs/deployster-k8s-clusterrolebinding:${BUILDKITE_COMMIT}
 docker tag quay.io/kfirs/deployster-k8s:${BUILDKITE_COMMIT} quay.io/kfirs/deployster-k8s-configmap:${BUILDKITE_COMMIT}
@@ -47,7 +47,7 @@ docker tag quay.io/kfirs/deployster-k8s:${BUILDKITE_COMMIT} quay.io/kfirs/deploy
 docker tag quay.io/kfirs/deployster-k8s:${BUILDKITE_COMMIT} quay.io/kfirs/deployster-k8s-storageclass:${BUILDKITE_COMMIT}
 
 # create k8s docker image aliases
-echo "--- Pushing resource images to registry"
+echo "+++ Pushing resource images to registry"
 docker push quay.io/kfirs/deployster-dresource:${BUILDKITE_COMMIT}
 docker push quay.io/kfirs/deployster-gcp:${BUILDKITE_COMMIT}
 docker push quay.io/kfirs/deployster-gcp-cloud-sql:${BUILDKITE_COMMIT}
