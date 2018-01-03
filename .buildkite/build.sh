d88947655166a3b0a8749246178f23f41c3ec233
Arik Kfir <arik@infolinks.com>
2018-01-03 15:24:29 +0200

    Replace manual pre-processing of resource Dockerfile(s) with build arguments in the "FROM" clause.
    Previously, to make the "FROM" clause dynamic, we've pre-processed all resource Dockerfile(s), via 'sed'. It seems, however, that Docker now supports this natively (see https://docs.docker.com/engine/reference/builder/#understand-how-arg-and-from-interact).

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -10,30 +10,33 @@ RELEASE_AND_SHA="${RELEASE}-${BUILDKITE_COMMIT}"
 echo "--- Login to Docker Hub"
 cat ${DOCKERHUB_PASSWORD_FILE} | docker login -u ${DOCKERHUB_USERNAME} --password-stdin
 
-echo "--- Updating FROM directives in Dockerfile(s)"
-for dockerfile in $(ls -X ./resources/Dockerfile.*); do
-    echo "Updating FROM directive in '${dockerfile}'"
-    sed "s/^FROM \(kfirz\/deployster-[^:]\+\):.\+$/FROM \1:${RELEASE_AND_SHA}/g" ${dockerfile} > ${dockerfile}.local
-done
-
 echo "--- Building 'dresource' Docker image"
-docker build -t kfirz/deployster-dresource:${RELEASE_AND_SHA} -f ./resources/Dockerfile.dresource.local ./resources
+docker build --build-arg "VERSION=${RELEASE_AND_SHA}" -t kfirz/deployster-dresource:${RELEASE_AND_SHA} -f ./resources/Dockerfile.dresource ./resources
+
 echo "--- Building 'gcp' Docker image"
-docker build -t kfirz/deployster-gcp:${RELEASE_AND_SHA} -f ./resources/Dockerfile.gcp.local ./resources
+docker build --build-arg "VERSION=${RELEASE_AND_SHA}" -t kfirz/deployster-gcp:${RELEASE_AND_SHA} -f ./resources/Dockerfile.gcp ./resources
+
 echo "--- Building 'gcp-cloud-sql' Docker image"
-docker build -t kfirz/deployster-gcp-cloud-sql:${RELEASE_AND_SHA} -f ./resources/Dockerfile.gcp_cloud_sql.local ./resources
+docker build --build-arg "VERSION=${RELEASE_AND_SHA}" -t kfirz/deployster-gcp-cloud-sql:${RELEASE_AND_SHA} -f ./resources/Dockerfile.gcp_cloud_sql ./resources
+
 echo "--- Building 'gcp-compute-ip-address' Docker image"
-docker build -t kfirz/deployster-gcp-compute-ip-address:${RELEASE_AND_SHA} -f ./resources/Dockerfile.gcp_compute_ip_address.local ./resources
+docker build --build-arg "VERSION=${RELEASE_AND_SHA}" -t kfirz/deployster-gcp-compute-ip-address:${RELEASE_AND_SHA} -f ./resources/Dockerfile.gcp_compute_ip_address ./resources
+
 echo "--- Building 'gcp-gke-cluster' Docker image"
-docker build -t kfirz/deployster-gcp-gke-cluster:${RELEASE_AND_SHA} -f ./resources/Dockerfile.gcp_gke_cluster.local ./resources
+docker build --build-arg "VERSION=${RELEASE_AND_SHA}" -t kfirz/deployster-gcp-gke-cluster:${RELEASE_AND_SHA} -f ./resources/Dockerfile.gcp_gke_cluster ./resources
+
 echo "--- Building 'gcp-iam-policy' Docker image"
-docker build -t kfirz/deployster-gcp-iam-policy:${RELEASE_AND_SHA} -f ./resources/Dockerfile.gcp_iam_service_account.local ./resources
+docker build --build-arg "VERSION=${RELEASE_AND_SHA}" -t kfirz/deployster-gcp-iam-policy:${RELEASE_AND_SHA} -f ./resources/Dockerfile.gcp_iam_service_account ./resources
+
 echo "--- Building 'gcp-iam-service-account' Docker image"
-docker build -t kfirz/deployster-gcp-iam-service-account:${RELEASE_AND_SHA} -f ./resources/Dockerfile.gcp_iam_policy.local ./resources
+docker build --build-arg "VERSION=${RELEASE_AND_SHA}" -t kfirz/deployster-gcp-iam-service-account:${RELEASE_AND_SHA} -f ./resources/Dockerfile.gcp_iam_policy ./resources
+
 echo "--- Building 'gcp-project' Docker image"
-docker build -t kfirz/deployster-gcp-project:${RELEASE_AND_SHA} -f ./resources/Dockerfile.gcp_project.local ./resources
+docker build --build-arg "VERSION=${RELEASE_AND_SHA}" -t kfirz/deployster-gcp-project:${RELEASE_AND_SHA} -f ./resources/Dockerfile.gcp_project ./resources
+
 echo "--- Building 'k8s' Docker image"
-docker build -t kfirz/deployster-k8s:${RELEASE_AND_SHA} -f ./resources/Dockerfile.k8s.local ./resources
+docker build --build-arg "VERSION=${RELEASE_AND_SHA}" -t kfirz/deployster-k8s:${RELEASE_AND_SHA} -f ./resources/Dockerfile.k8s ./resources
+
 echo "--- Building 'deployster' image"
 docker build --build-arg "VERSION=${RELEASE_AND_SHA}" --tag kfirz/deployster:${RELEASE_AND_SHA} -f ./Dockerfile .
 
@@ -98,8 +101,6 @@ if [[ -z "${LOCAL+x}" ]]; then
     docker push kfirz/deployster-k8s-serviceaccount:${RELEASE_AND_SHA}
     docker push kfirz/deployster-k8s-statefulset:${RELEASE_AND_SHA}
     docker push kfirz/deployster-k8s-storageclass:${RELEASE_AND_SHA}
-
-    echo "--- Pushing main Deployster image"
     docker push kfirz/deployster:${RELEASE_AND_SHA}
 fi
 
