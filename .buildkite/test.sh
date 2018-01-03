479e4f04da476ff23bcfbfd8a9579e1802a21ff3
Arik Kfir <arik@infolinks.com>
2018-01-03 14:59:42 +0200

    Refactor build scripts for the new developer story using develobot.

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-set -e
+set -euo pipefail
+RELEASE_AND_SHA="${RELEASE:-0.0.0}-${BUILDKITE_COMMIT:-local}"
 
 echo "--- Building test image"
-docker build --file ./Dockerfile.test --tag infolinks/deployster:test-${BUILDKITE_COMMIT:-0.0.0} .
+docker build --file ./Dockerfile.test --tag infolinks/deployster-tests:${RELEASE_AND_SHA} .
 
-echo "+++ Running tests"
+echo "--- Running tests"
 docker run --env COVERALLS_REPO_TOKEN="${COVERALLS_REPO_TOKEN}" \
            --env BUILDKITE="${BUILDKITE}" \
            --env BUILDKITE_JOB_ID="${BUILDKITE_JOB_ID}" \
            --env BUILDKITE_BRANCH="${BUILDKITE_BRANCH}" \
            --tty \
-           infolinks/deployster:test-${BUILDKITE_COMMIT:-0.0.0} \
+           infolinks/deployster-tests:${RELEASE_AND_SHA} \
            $@
