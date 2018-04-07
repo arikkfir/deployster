4c210b0f303ad7e6987c8d3bd6907b9150c075de
Arik Kfir <arik@kfirs.com>
2018-04-07 19:35:34 +0300

    Think I got it now

--- .buildkite/generate-pipeline.sh
+++ .buildkite/generate-pipeline.sh
@@ -36,8 +36,7 @@ echo "    plugins:"
 echo "      docker-login#v1.0.0:"
 echo "        username: arikkfir"
 echo "    command:"
-echo "      - set -ex"
-echo '      - sed -i "s/^FROM \(kfirz\/.\+\):.\+/FROM \1:${TAG}/" ./resources/Dockerfile.gcp'
+echo '      - sed -i "s/^FROM \(kfirz\/.\+\):.\+/FROM \1:'${TAG}'/" ./resources/Dockerfile.gcp'
 echo "      - $(build_resource gcp)"
 echo "      - docker push kfirz/deployster-gcp:${TAG}"
 echo "  - label: Build \"k8s\" resource images"
@@ -45,8 +44,7 @@ echo "    plugins:"
 echo "      docker-login#v1.0.0:"
 echo "        username: arikkfir"
 echo "    command:"
-echo "      - set -ex"
-echo '      - sed -i "s/^FROM \(kfirz\/.\+\):.\+/FROM \1:${TAG}/" ./resources/Dockerfile.k8s'
+echo '      - sed -i "s/^FROM \(kfirz\/.\+\):.\+/FROM \1:'${TAG}'/" ./resources/Dockerfile.k8s'
 echo "      - $(build_resource k8s)"
 echo "      - docker push kfirz/deployster-k8s:${TAG}"
 echo "  - wait"
@@ -56,8 +54,7 @@ for name in "gcp-cloud-sql" "gcp-compute-ip-address" "gcp-gke-cluster" "gcp-iam-
     echo "      docker-login#v1.0.0:"
     echo "        username: arikkfir"
     echo "    command:"
-    echo "      - set -ex"
-    echo '      - sed -i "s/^FROM \(kfirz\/.\+\):.\+/FROM \1:${TAG}/" ./resources/Dockerfile.'${name}
+    echo '      - sed -i "s/^FROM \(kfirz\/.\+\):.\+/FROM \1:'${TAG}'/" ./resources/Dockerfile.'${name}
     echo "      - $(build_resource ${name})"
     echo "      - docker push kfirz/deployster-${name}:${TAG}"
 done
@@ -67,8 +64,7 @@ for name in "clusterrole" "clusterrolebinding" "configmap" "cronjob" "daemonset"
     echo "      docker-login#v1.0.0:"
     echo "        username: arikkfir"
     echo "    command:"
-    echo "      - set -ex"
-    echo '      - sed -i "s/^FROM \(kfirz\/.\+\):.\+/FROM \1:${TAG}/" ./resources/Dockerfile.'${name}
+    echo '      - sed -i "s/^FROM \(kfirz\/.\+\):.\+/FROM \1:'${TAG}'/" ./resources/Dockerfile.'${name}
     echo "      - docker tag kfirz/deployster-k8s:${TAG} kfirz/deployster-k8s-${name}:${TAG}"
     echo "      - docker push kfirz/deployster-k8s-${name}:${TAG}"
 done
