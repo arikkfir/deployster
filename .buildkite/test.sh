8529004303bd7ed13ca89f564ecd74e514f50c0e
Arik Kfir <arik@kfirs.com>
2018-04-07 16:46:48 +0300

    Fix "test.sh"

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-RELEASE_AND_SHA="${RELEASE:-0.0.0}-${BUILDKITE_COMMIT:-local}"
 
 echo "--- Building test image"
-docker build --file ./Dockerfile.test --tag infolinks/deployster-tests:${RELEASE_AND_SHA} .
+docker build --file ./Dockerfile.test --tag infolinks/deployster-tests:${BUILDKITE_COMMIT:-local} .
 
 echo "--- Running tests"
-docker run --env COVERALLS_REPO_TOKEN="${COVERALLS_REPO_TOKEN}" \
+docker run --env COVERALLS_REPO_TOKEN="$(cat /buildkite/secrets/coveralls-token.txt)" \
            --env BUILDKITE="${BUILDKITE}" \
            --env BUILDKITE_JOB_ID="${BUILDKITE_JOB_ID}" \
            --env BUILDKITE_BRANCH="${BUILDKITE_BRANCH}" \
            --tty \
-           infolinks/deployster-tests:${RELEASE_AND_SHA} \
+           infolinks/deployster-tests:${BUILDKITE_COMMIT:-local} \
            $@
