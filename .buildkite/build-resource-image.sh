e49b6222a6dcb5d814ff24a1584b78cdae8e2629
Arik Kfir <arik@kfirs.com>
2018-04-19 00:06:20 +0300

    Support accepting custom Dockerfile path for resource image

--- .buildkite/build-resource-image.sh
+++ .buildkite/build-resource-image.sh
@@ -12,10 +12,19 @@ if [[ -z "${TAG}" ]]; then
     exit 1
 fi
 
+DOCKERFILE="${3}"
+if [[ -z "${DOCKERFILE}" ]]; then
+    DOCKERFILE="./resources/Dockerfile.${RESOURCE}"
+fi
+if [[ ! -f "${DOCKERFILE}" ]]; then
+    echo "could not find '${DOCKERFILE}'" >&2
+    exit 1
+fi
+
 set -ex
 
-sed -i "s/^FROM \(kfirz\/.\+\):.\+/FROM \1:'${TAG}'/" ./resources/Dockerfile.${RESOURCE}
-docker build --build-arg "VERSION=${TAG}" -t kfirz/deployster-${RESOURCE}:${TAG} -f ./resources/Dockerfile.${RESOURCE} ./resources
+sed -i "s/^FROM \(kfirz\/.\+\):.\+/FROM \1:'${TAG}'/" ${DOCKERFILE}
+docker build --build-arg "VERSION=${TAG}" -t kfirz/deployster-${RESOURCE}:${TAG} -f ${DOCKERFILE} ./resources
 docker push kfirz/deployster-${RESOURCE}:${TAG}
 
 exit 0
