e807b485b20993a9dbed9c01293b82a673156d8d
Arik Kfir <arik@kfirs.com>
2018-04-18 23:24:50 +0300

    Fix building the "dresource" image

--- .buildkite/generate-resource-images-pipeline.sh
+++ .buildkite/generate-resource-images-pipeline.sh
@@ -8,12 +8,12 @@ fi
 
 echo "steps:"
 for name in "gcp-cloud-sql" "gcp-compute-ip-address" "gcp-gke-cluster" "gcp-iam-policy" "gcp-iam-service-account" "gcp-project"; do
-    echo "  - label: Build \"kfirz/deployster-${name}\""
+    echo "  - label: kfirz/deployster-${name}"
     echo "    plugins: { docker-login#v1.0.0: { username: arikkfir} }"
     echo "    command: ./.buildkite/build-resource-image.sh ${name} ${TAG}"
 done
 for name in "clusterrole" "clusterrolebinding" "configmap" "cronjob" "daemonset" "deployment" "horizontalpodautoscaler" "ingress" "job" "namespace" "networkpolicy" "node" "persistentvolume" "persistentvolumeclaim" "pod" "replicaset" "replicationcontroller" "role" "rolebinding" "secret" "service" "serviceaccount" "statefulset" "storageclass"; do
-    echo "  - label: Build \"kfirz/deployster-k8s-${name}\""
+    echo "  - label: kfirz/deployster-k8s-${name}"
     echo "    plugins: { docker-login#v1.0.0: { username: arikkfir} }"
     echo "    command: ./.buildkite/build-resource-image.sh k8s-${name} ${TAG}"
 done
